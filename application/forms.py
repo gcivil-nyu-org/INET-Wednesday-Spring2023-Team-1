9968e524175692dcc102e7a7fdbca0158a00ff24
lillyengeler <lre250@nyu.edu>
2023-03-20 11:52:15 -0400

    login and register connecting static

--- application/forms.py
+++ application/forms.py
@@ -16,8 +16,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'background-color': 'red'}))
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    password1 = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Confirm Password'}))
+    
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
